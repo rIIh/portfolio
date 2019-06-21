8a87861b3265f1af7c41dc8e15e74699839a9a71
riih <ratealt@gmail.com>
2019-06-22 02:11:23 +0300

    0.0.2

---
+++ src/components/OverlaySlider.vue
@@ -0,0 +1,52 @@
+<template lang="pug">
+  .overlay-slider(:style='style')
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue, Watch } from 'vue-property-decorator';
+import { lerp } from '@/api/scripts';
+import { stat } from 'fs';
+
+@Component({})
+export default class Home extends Vue {
+  @Prop({required: true})
+  private getter: any;
+  private state = this.getter;
+
+  private get getState(){
+    if (this.state === null) { return 0; }
+    return this.state();
+  }
+
+  private get style() {
+    let value: number;
+    const scrollPerPage = window.innerHeight;
+    if (this.getState < 0.5) {
+      value = lerp(0, 30, this.getState * 2);
+    } else {
+      value = lerp(30, 0, this.getState * 2 - 1);
+    }
+    const path =
+      'polygon(0 ' +
+      value +
+      '%, 100% 0%, 100% ' +
+      (100 - value) +
+      '%, 0% 100%)';
+    return {
+      'top': scrollPerPage - 2 * this.getState * scrollPerPage + 'px',
+      'clip-path': path,
+    };
+  }
+}
+</script>
+
+
+<style lang="sass">
+.overlay-slider
+  position: absolute
+  width: 100vw
+  height: 100vh
+  left: 0
+  z-index: 100
+  background: white
+</style>
---
+++ src/components/Timeline.vue
@@ -0,0 +1,41 @@
+<template lang="pug">
+  .timeline
+    swiper()
+      swiper-slide(v-for="slide in getDataSwiper.slides" :key="")
+        h1 test
+</template>
+
+<style lang="sass">
+.timeline
+  position: fixed
+  top: 0
+  left: 0
+  width: 150px
+  height: 100%
+  background: grey
+  z-index: 50
+</style>
+
+<script lang="ts">
+import { Component, Prop, Vue, Watch } from 'vue-property-decorator';
+
+@Component({
+  components: {
+  }
+})
+export default class Home extends Vue {
+  @Prop()
+  private data: any[] | undefined;
+
+
+
+  private get getDataSwiper(){
+    if(this.data !== undefined){
+      return this.data;
+    }
+    return {
+      slides: []
+    }
+  }
+}
+</script>
